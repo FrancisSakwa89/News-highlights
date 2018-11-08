435947cc96b63f5b692e8e0e50bc75dd05e7a41c
sakwafrancis89@gmail.com <sakwafrancis89@gmail.com>
2018-11-08 19:03:41 +0300

    modified app folder with its subfolders by adding an error page of 404 to errors.py

--- app/main/views.py
+++ app/main/views.py
@@ -11,3 +11,20 @@ def index():
 
     message = 'Hello World'
     return render_template('index.html',message = message)der_template('index.html')
+
+@app.route('/news/<news_id>')
+def movie(news_id):
+
+    '''
+    View movie page function that returns the news details page and its data
+    '''
+    return render_template('news.html',id = news_id)    
+
+@app.route('/news/<int:news_id>')
+def movie(news_id):
+
+    '''
+    View movie page function that returns the news details page and its data
+    '''
+    return render_template('movie.html',id = news_id)
+    
